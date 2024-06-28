3e218a072ec58570dea4fa873ddf8af8607890c6
Ilya Buravov <ilburale@gmail.com>
2024-06-28 12:10:04 +0700

    lil updates

--- runtime/include/oge/core/platform.h
+++ runtime/include/oge/core/platform.h
@@ -14,15 +14,17 @@ void  _ogePlatformTerminate();
 
 void  _ogePlatformPumpMessages();
 
+b8    _ogePlatformTerminateRequested();
+
 void* _ogePlatformAllocate(u64 size, b8 aligned);
 void  _ogePlatformDeallocate(void *block, b8 aligned);
-void  _ogePlatformMemoryCopy(void *dst, void *src, u64 size);
+void  _ogePlatformMemoryCopy(void *dst, const void *src, u64 size);
 void  _ogePlatformMemorySet(void *dst, i32 value, u64 size);
 
 void  _ogePlatformConsoleWrite(const char *msg, u8 clr);
 
 /**
- * @brief Returns amount of ellapsed milliseconds since the unix epoch.
+ * @brief Returns amount of seconds past since the unix epoch.
  */
 u64   _ogePlatformGetTime();
 
